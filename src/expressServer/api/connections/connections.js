a01973edebafff7ab9555d80bd2f2beaf24220d9
atlasAir <gab.maglia@gmail.com>
2022-11-16 18:41:48 -0300

    serverUload

---
+++ src/expressServer/api/connections/connections.js
@@ -0,0 +1,24 @@
+
+const mysql = require("mysql2");
+
+
+
+const mysqlConnection = mysql.createConnection(({
+    host: 'localhost',
+    user: 'root',
+    password: 'mysql',
+    database: "portfolio",
+    port: "3306"
+
+}));
+
+mysqlConnection.connect( err => {
+    if (err){
+        console.log(`Error en db : ${err}`)
+        return;
+    }else {
+        console.log(`db Ok!`)
+    }
+});
+
+module.exports = mysqlConnection;

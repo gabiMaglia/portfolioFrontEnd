17b17fbda95152e3482b83eeca755855dd1148ab
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2022-11-26 19:09:07 -0300

    HomeImplemented

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from 'express';
+
 
 @Injectable({
   providedIn: 'root'
@@ -18,20 +18,6 @@ export class AuthService {
   
   }
 
-
-
-
-  // login(usuario: string, password: string ) {
-  //   this.http.post(this.api  + '/authenticate', {usuario: usuario, password: password})
-  //             .subscribe((resp: any) => {
-  //               //Redireccionamos al usuario a su perfil
-  //                 (<any> this.router).navigate(['profile'])
-  //                 //Guardamos el token en localStorage
-  //                 localStorage.setItem('auth_token', resp.token)
-  
-  //             });
-              
-  // }
   logout() {
     localStorage.removeItem('token')
   }
